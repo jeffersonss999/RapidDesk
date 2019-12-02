a09cecfbf724f1a9621ef69fb7f9e86e68e17690
Jefferson <jeffersonss999@gmail.com>
2019-12-02 05:23:13 -0300

    Implementação do JavaScript

--- RapidDesk/assets/javascript/index.js
+++ RapidDesk/assets/javascript/index.js
@@ -1,1 +1,13 @@
-console.log("index.js")
+console.log("index.js");
+
+function inserirMascara(campoPassado, mascara) {
+    console.log(campoPassado.value.length)
+    console.log(campoPassado.value)
+    var campo = campoPassado.value.length;
+    var saida = mascara.substring(0,1);
+    var texto = mascara.substring(campo);
+
+    if (texto.substring(0, 1) != saida) {
+        campoPassado.value += texto.substring(0, 1);
+    }
+}
